e8cbe0ef3b12326985b762bcaafac64ba712580c
Grisha <just.grishaayf@gmail.com>
2023-03-20 16:42:57 +0300

    Increse decrease count

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  console.log(count);
+
+  function increament() {
+    setCount(count + 1);
+  }
+  function decrement() {
+    setCount(count -1);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{count}</h1>
+      <button onClick={increament}>Increasment</button>
+      <button onClick={decrement}>Decreasment</button>
     </div>
   );
 }
